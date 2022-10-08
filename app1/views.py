3ef5a23e40a88a8640e9d70fdc812094610294f0
maneeshasinoj <sinojmaneesha@gmail.com>
2022-10-08 16:37:46 +0530

    7th

--- app1/views.py
+++ app1/views.py
@@ -10394,33 +10394,19 @@ def samplereceipt(request):
     no=receipt.id
     no=no+1
     print(no)
-    return render(request,'samplereceipt.html',{'ledger':ledger,'no':no})
+    return render(request,'samplere.html',{'ledger':ledger,'no':no})
+
+
+def sample_receipt(request):
+    ledger=tally_ledger.objects.all()
+    receipt=receiptvoucher.objects.last()
+    no=receipt.id
+    no=no+1
+    print(no)
+    return render(request,'receiptvouchersample.html',{'ledger':ledger,'no':no})
 
 
 
-
-def purchase(request):#ann
-    return render(request,'sale.html')  
-
-
-def purchase_add(request):#ann
- if request.method=="POST":
-    supplierinvoiceno=request.POST.get('Suppliernumber') 
-    partyAc=request.POST.get('Party_name') 
-    purchase_date=request.POST.get('Date')
-    currentbalancep =request.POST.get('current_ac_balancep')
-    currentbalancepl =request.POST.get('current_ac_balancel')
-    quantity1=request.POST.get('quantity')
-    price1=request.POST.get('rate')
-    total1=request.POST.get('amount')
-    nameofitem=request.POST.get('Item_name')
-    purchasele=request.POST.get('purchaseledger')
-    purchase=Purchase(supplierinvoiceno=supplierinvoiceno,partyAccntname=partyAc,purchase_date=purchase_date,purchaseledger=purchasele,currentbalancepl =currentbalancepl ,currentbalancep=currentbalancep,nameofitem=nameofitem,quantity=quantity1,price=price1,total=total1)
-    purchase.save()
-    print("purchase")
-    messages.info(request,'Purchase entered successfully')
-    return render(request,'sale.html',)    
-    #return redirect('/')       
 
 
 
